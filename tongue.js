2ba6b71a87f04a846ee2ca3f6df0a3c9580fee2d
youngwasd <youngk126621@gmail.com>
2024-01-04 20:18:34 -0800

    little thing

--- tongue.js
+++ tongue.js
@@ -1,17 +1,19 @@
 class Tongue {
     constructor(game) {
         this.game = game;
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./tongue.png"), 1.5, 0, 56.3, 55, 7, 0.3);
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./tongue.png"), 1.5, 0, 56.3, 55, 7, 0.2);
 
         this.x = 1005;
         this.y = 678;
-        this.speed = 45;
+        this.speed = 100;
     };
 
     update() {
         this.x -= this.speed * this.game.clockTick;
 
         if (this.x <= -100) this.x = 1005;
+
+        this.game.clockTick < 0.005 ? this.speed = 5 : this.speed = 100;
     };
 
     draw(ctx) {
